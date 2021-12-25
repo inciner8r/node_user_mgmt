3efa0cc3d198242fe13174effbc3cec42c2b81f1
inciner8r <punarvpawade17@gmail.com>
2021-12-25 18:30:56 +0530

    view engine, morgan, express body parser

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require("express");
-const res = require("express/lib/response");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
 
 dotenv.config({ path: "config.env" });
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+
+//log request with morgan
+app.use(morgan("tiny"));
+
+//parse body
+app.use(express.urlencoded({ extended: true }));
+
+//view engine
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
   res.send("crud app");
 });
