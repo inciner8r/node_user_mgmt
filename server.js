ee9c85958f2dee67e7cd5e7b3fb36f438e25afe2
inciner8r <punarvpawade17@gmail.com>
2021-12-26 22:23:01 +0530

    ignore env, render and route

--- server.js
+++ server.js
@@ -23,15 +23,8 @@ app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
 app.use("/img", express.static(path.resolve(__dirname, "assets/img")));
 app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
-app.get("/add-user", (req, res) => {
-  res.render("add_user");
-});
-app.get("/update-user", (req, res) => {
-  res.render("update_user");
-});
+//load router
+app.use("/", require("./server/routes/router"));
 
 app.listen(PORT, () => {
   console.log("server running at 3000");
