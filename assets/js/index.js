e6ec446692378d19352cadf0b191dbe31701d1d6
inciner8r <punarvpawade17@gmail.com>
2021-12-28 19:26:47 +0530

    update user final

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,23 @@
 function add_user_btn(e) {
   alert("data inserted");
 }
+
+$("#update_user").submit(function (event) {
+  event.preventDefault();
+  let tempArray = $(this).serializeArray();
+  let data = {};
+  $.map(tempArray, function (n, i) {
+    data[n["name"]] = n["value"];
+  });
+  console.log(tempArray);
+
+  let request = {
+    url: `http://localhost:3000/api/users/${data.id}`,
+    method: "PUT",
+    data: data,
+  };
+
+  $.ajax(request).done(function (respnse) {
+    alert("data updated");
+  });
+});
