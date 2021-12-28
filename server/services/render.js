b3fa0dc25ddb6b88d7fff31f223b035dd483d3de
inciner8r <punarvpawade17@gmail.com>
2021-12-28 14:31:32 +0530

    update user form and misc fixes

--- server/services/render.js
+++ server/services/render.js
@@ -15,5 +15,14 @@ exports.add_user = (req, res) => {
   res.render("add_user");
 };
 exports.update_user = (req, res) => {
-  res.render("update_user");
+  axios
+    .get("http://localhost:3000/api/users", {
+      params: { id: req.query.id },
+    })
+    .then((userdata) => {
+      res.render("update_user", { user: userdata.data });
+    })
+    .catch((err) => {
+      res.send(err);
+    });
 };
