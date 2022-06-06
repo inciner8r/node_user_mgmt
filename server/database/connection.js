81e1bbfd8091220ed023c01e16d38e4b3d9eff51
Punarv Pawade <Punarvpawade17@gmail.com>
2022-06-07 03:23:56 +0530

    dockerfile

--- server/database/connection.js
+++ server/database/connection.js
@@ -2,10 +2,13 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const con = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      UseUnifiedTopology: true,
-    });
+    const con = await mongoose.connect(
+      "mongodb://admin:password@localhost:27017",
+      {
+        useNewUrlParser: true,
+        UseUnifiedTopology: true,
+      }
+    );
     console.log(`mongoDB connected: ${con.connection.host}`);
   } catch (err) {
     console.log(err);
